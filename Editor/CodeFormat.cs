937fa9a65c4e8baf78f04ea32dcdacd6ab66ec0c
dpull <beingstudio@gmail.com>
2015-01-30 11:55:19 +0800

    整理代码

---
+++ Editor/CodeFormat.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public static class CodeFormat
+{
+	[MenuItem("XStudio/Tools/Code Format")]
+	public static void Process()
+	{
+		var srcs = System.IO.Directory.GetFiles("Assets/Component", "*.cs", System.IO.SearchOption.AllDirectories);
+		var utf8Encoding = new System.Text.UTF8Encoding(true);
+		
+		foreach(var src in srcs)
+		{
+			var auto = System.IO.File.ReadAllText(src);
+			auto = auto.Replace("\r\n", "\n");
+			System.IO.File.WriteAllText(src, auto, utf8Encoding);
+		}
+		Debug.Log("Code format finish");
+	}
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# unity-tools #
+
+是开发中用到的一些和项目无关的小工具
+
+## UIAdapterEditor ##
+
+用来做NGUI适配
+
+## CodeFormat ##
+
+解决不同程序使用mac和windows系统开发引入的一些问题：
+
+1. 换行符到底是\r\n 还是 \n
+1. 文件中的中文是不是utf8
+1. 文件有没有unf8 bom头
